64fb07ee10b3cd0b9dc8b42891c1d40f40e70dda
BG <Bgee269@hotmail.com>
2019-04-27 19:33:09 -0400

    updated

--- app/config/connection.js
+++ app/config/connection.js
@@ -22,25 +22,25 @@ var sequelize = new Sequelize("sequelize_chirpy1", "root", "", {
 
 
 
-var mysql = require("mysql");
+//var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Charmgizmo3!",
-  database: "chirpy"
-});
+//var connection = mysql.createConnection({
+ // port: 3306,
+ // host: "localhost",
+ // user: "root",
+ // password: "Charmgizmo3!",
+ // database: "chirpy"
+//});
 
 // Connect to the database
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+//connection.connect(function(err) {
+ // if (err) {
+ //   console.error("error connecting: " + err.stack);
+  //  return;
+ // }
+ // console.log("connected as id " + connection.threadId);
+//});
 
 // Export connection
-module.exports = connection;
+//module.exports = connection;
